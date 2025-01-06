13b7d056a904e14c8475d204a5967dae3b364051
kaisck <chaniourkais@gmail.com>
2025-01-06 10:57:36 +0100

    firebase corrected but add closed after save name

--- app/src/main/java/com/example/clickgame/LeaderboardScreen.kt
+++ app/src/main/java/com/example/clickgame/LeaderboardScreen.kt
@@ -1,14 +1,14 @@
 package com.example.clickgame
-import androidx.compose.runtime.remember
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
 @Composable
 fun LeaderboardScreen(leaderboard: SnapshotStateList<Pair<String, Int>>) {
     var isLoading by remember { mutableStateOf(true) }
